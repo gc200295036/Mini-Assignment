5a31b7558cb43d230ca49f1d37674a7d7423dfc5
gc200295036 <44586823+gc200295036@users.noreply.github.com>
2018-12-10 00:36:25 -0500

    Add files via upload
    Mini Assignment
    (First Commit)

---
+++ miniAssignment-NickBrazda/miniAssignment-NickBrazda/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace miniAssignment_NickBrazda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblGrade = new System.Windows.Forms.Label();
+            this.lblGrade2 = new System.Windows.Forms.Label();
+            this.lblGrade3 = new System.Windows.Forms.Label();
+            this.lblGrade4 = new System.Windows.Forms.Label();
+            this.lblGrade5 = new System.Windows.Forms.Label();
+            this.lblGrade6 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(44, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(44, 69);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(44, 95);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(44, 121);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(44, 147);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblGrade
+            // 
+            this.lblGrade.AutoSize = true;
+            this.lblGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGrade.Location = new System.Drawing.Point(3, 12);
+            this.lblGrade.Name = "lblGrade";
+            this.lblGrade.Size = new System.Drawing.Size(41, 12);
+            this.lblGrade.TabIndex = 7;
+            this.lblGrade.Text = "Class #1";
+            // 
+            // lblGrade2
+            // 
+            this.lblGrade2.AutoSize = true;
+            this.lblGrade2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGrade2.Location = new System.Drawing.Point(3, 43);
+            this.lblGrade2.Name = "lblGrade2";
+            this.lblGrade2.Size = new System.Drawing.Size(41, 12);
+            this.lblGrade2.TabIndex = 8;
+            this.lblGrade2.Text = "Class #2";
+            // 
+            // lblGrade3
+            // 
+            this.lblGrade3.AutoSize = true;
+            this.lblGrade3.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGrade3.Location = new System.Drawing.Point(3, 69);
+            this.lblGrade3.Name = "lblGrade3";
+            this.lblGrade3.Size = new System.Drawing.Size(41, 12);
+            this.lblGrade3.TabIndex = 9;
+            this.lblGrade3.Text = "Class #3";
+            // 
+            // lblGrade4
+            // 
+            this.lblGrade4.AutoSize = true;
+            this.lblGrade4.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGrade4.Location = new System.Drawing.Point(3, 147);
+            this.lblGrade4.Name = "lblGrade4";
+            this.lblGrade4.Size = new System.Drawing.Size(41, 12);
+            this.lblGrade4.TabIndex = 10;
+            this.lblGrade4.Text = "Class #6";
+            // 
+            // lblGrade5
+            // 
+            this.lblGrade5.AutoSize = true;
+            this.lblGrade5.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGrade5.Location = new System.Drawing.Point(3, 95);
+            this.lblGrade5.Name = "lblGrade5";
+            this.lblGrade5.Size = new System.Drawing.Size(41, 12);
+            this.lblGrade5.TabIndex = 11;
+            this.lblGrade5.Text = "Class #4";
+            // 
+            // lblGrade6
+            // 
+            this.lblGrade6.AutoSize = true;
+            this.lblGrade6.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGrade6.Location = new System.Drawing.Point(3, 121);
+            this.lblGrade6.Name = "lblGrade6";
+            this.lblGrade6.Size = new System.Drawing.Size(41, 12);
+            this.lblGrade6.TabIndex = 12;
+            this.lblGrade6.Text = "Class #5";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(44, 261);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 14;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(44, 174);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 15;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(44, 201);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Total:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 12);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Average:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(48, 284);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 12);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "GPA - LetterGrade";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(192, 297);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.lblGrade6);
+            this.Controls.Add(this.lblGrade5);
+            this.Controls.Add(this.lblGrade4);
+            this.Controls.Add(this.lblGrade3);
+            this.Controls.Add(this.lblGrade2);
+            this.Controls.Add(this.lblGrade);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblGrade;
+        private System.Windows.Forms.Label lblGrade2;
+        private System.Windows.Forms.Label lblGrade3;
+        private System.Windows.Forms.Label lblGrade4;
+        private System.Windows.Forms.Label lblGrade5;
+        private System.Windows.Forms.Label lblGrade6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ miniAssignment-NickBrazda/miniAssignment-NickBrazda/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//miniAssignment-NickBrazda - Programming Fundamentals.
+namespace miniAssignment_NickBrazda
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Create Variable
+        int class1, class2, class3, class4, class5, class6, tot, avg;
+        //  button calculate grade 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            class1 = int.Parse(textBox1.Text);
+            class2 = int.Parse(textBox2.Text);
+            class3 = int.Parse(textBox3.Text);
+            class4 = int.Parse(textBox4.Text);
+            class5 = int.Parse(textBox5.Text);
+            class6 = int.Parse(textBox6.Text);
+
+            tot = class1 + class2 + class3 + class4 + class5 + class6;
+
+            textBox8.Text = tot.ToString();
+
+            avg = tot / 6;
+
+            textBox9.Text = avg.ToString();
+
+            if (avg > 79)
+            {
+                textBox7.Text = "A";
+            }
+            else if (avg > 69)
+            {
+                textBox7.Text = "B";
+            }
+            else if (avg > 59)
+            {
+                textBox7.Text = "C";
+            }
+            else if (avg > 49)
+            {
+                textBox7.Text = "D";
+            }
+            else
+            {
+                textBox7.Text = "F";
+            }
+            }
+        }
+    }
+
+
